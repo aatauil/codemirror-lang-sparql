f1417ac191e7f44d7389bbb63b7b935211dc873e
Achraf Atauil <achrafatauil@hotmail.com>
2023-02-21 20:23:59 +0100

    update package.json & rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,12 @@
-import {nodeResolve} from "@rollup/plugin-node-resolve"
+import typescript from "rollup-plugin-ts"
+import {lezer} from "@lezer/generator/rollup"
 
 export default {
-  input: "./src/parser.js",
-  output: [{
-    format: "cjs",
-    file: "./dist/index.cjs"
-  }, {
-    format: "es",
-    file: "./dist/index.es.js"
-  }],
-  external(id) { return !/^[\.\/]/.test(id) },
-  plugins: [
-    nodeResolve()
-  ]
+  input: "src/index.ts",
+  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  output: [
+    {file: "dist/index.cjs", format: "cjs"},
+    {dir: "./dist", format: "es"}
+  ],
+  plugins: [lezer(), typescript()]
 }
