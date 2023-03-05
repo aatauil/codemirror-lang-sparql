0d519b9e5cf352b95bbdb0b9cf437eab9aef32c6
Achraf Atauil <achrafatauil@hotmail.com>
2023-03-05 16:28:55 +0100

    add styleTags (incomplete but its a start)

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,23 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import { parser } from "./syntax.grammar"
+import { LRLanguage, LanguageSupport } from "@codemirror/language"
+import { styleTags, tags as t } from "@lezer/highlight"
 
 export const SparqlLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
       styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
+        Comment: t.comment,
+        "BASE PREFIX SELECT CONSTRUCT DESCRIBE WHERE DISTINCT REDUCED STR LANG LANGMATCHES DATATYPE ASK BOUND IRI URI BNODE RAND ABS CEIL FLOOR ROUND CONCAT STRLEN UCASE LCASE ENCODE_FOR_URI CONTAINS STRSTARTS STRENDS STRBEFORE STRAFTER YEAR MONTH DAY HOURS MINUTES SECONDS TIMEZONE TZ NOW UUID STRUUID MD5 SHA1 SHA256 SHA384 SHA512 COALESCE IF STRLANG STRDT SAMETERM ISIRI ISURI ISBLANK ISLITERAL ISNUMERIC COUNT SUM MIN MAX AVG SAMPLE GROUP_CONCAT SEPARATOR SUBSTR REPLACE REGEX EXISTS NOT IN GROUP BY HAVING ORDER ASC DESC LIMIT OFFSET VALUES UNDEF UNION OPTIONAL MINUS GRAPH SERVICE SILENT FILTER BIND AS FROM NAMED LOAD INTO TO CLEAR DROP CREATE ADD MOVE COPY INSERT DATA DELETE WITH DEFAULT ALL USING": t.keyword,
+        "Var ObjectListPath/..": t.variableName,
         String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
+        Integer: t.integer,
+        "Double Decimal": t.float,
+        "{ }": t.brace,
+        Langstag: t.annotation,
+        "TRUE FALSE": t.bool
       })
     ]
   }),
-  languageData: {
-    commentTokens: {line: ";"}
-  }
 })
 
 export function sparql() {
