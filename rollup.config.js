bf5c8a7d3d9ebaef197ca159cd879b46c509b2cf
Achraf <achrafatauil@hotmail.com>
2026-04-01 15:04:28 +0200

    Update devDeps, rollup & tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,23 @@
-import typescript from "rollup-plugin-ts"
+import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
 import {lezer} from "@lezer/generator/rollup"
 
-export default {
-  input: "src/index.ts",
-  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
-  output: [
-    {file: "dist/index.cjs", format: "cjs"},
-    {dir: "./dist", format: "es"}
-  ],
-  plugins: [lezer(), typescript()]
-}
+const external = id => !/^(\.?\/|\w:)/.test(id)
+
+export default [
+  {
+    input: "src/index.ts",
+    external,
+    output: [
+      {file: "dist/index.cjs", format: "cjs"},
+      {dir: "./dist", format: "es"}
+    ],
+    plugins: [lezer(), typescript()]
+  },
+  {
+    input: "src/index.ts",
+    external,
+    output: [{file: "dist/index.d.ts", format: "es"}],
+    plugins: [dts()]
+  }
+]
